49951c0b54bfe4e466718af2faba44f51f7ba23e
kartikrajjjj <kartikraj9756@gmail.com>
2026-06-02 00:39:17 +0530

    Learned about fragements

---
+++ 18_fragements/learning-fragements/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <React.Fragment>
+    <h1>Healthy Food Items</h1>
+    <ul class="list-group">
+  <li class="list-group-item">Vegetables</li>
+  <li class="list-group-item">Fruits</li>
+  <li class="list-group-item">Whole Grains</li>
+  <li class="list-group-item">Lean protein</li>
+  <li class="list-group-item">Healthy fats</li>
+</ul>
+</React.Fragment>
+  )
+}
+
+export default App
---
+++ 18_fragements/short-syntax/src/App.jsx
@@ -0,0 +1,19 @@
+//no need to import React and using <></> instead of <React.fragement></React.fragement>
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Healthy foods to eat</h1>
+      <ul class="list-group">
+        <li class="list-group-item">Pulses</li>
+        <li class="list-group-item">Green leafy vegetables</li>
+        <li class="list-group-item">Eggs</li>
+        <li class="list-group-item">Fruits</li>
+        <li class="list-group-item">Chicken</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
