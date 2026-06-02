7332ffb2a7b0aca5fdf8af363489f1f01fd29ea2
kartikrajjjj <kartikraj9756@gmail.com>
2026-06-03 01:12:57 +0530

    Practice component styling with CSS Modules

---
+++ 6_css_modules/src/App.jsx
@@ -0,0 +1,13 @@
+import Heading from "./components/Heading"
+import About from "./components/About"
+function App() {
+
+  return (
+    <>
+      <Heading></Heading>
+      <About></About>
+    </>
+  )
+}
+
+export default App
---
+++ 6_css_modules/src/components/About.jsx
@@ -0,0 +1,18 @@
+import styles from "./About.module.css"
+function About() {
+  return (
+    <p className={styles.AboutMe}>
+      I'm a Computer Science engineering student passionate about software
+      development, problem-solving, and building real-world applications.
+      Currently, I'm focused on mastering Data Structures & Algorithms, React,
+      and the MERN stack while preparing for software engineering placements. I
+      enjoy turning ideas into projects, continuously improving my coding
+      skills, and learning modern web technologies. My goal is to become a
+      skilled full-stack developer capable of building scalable applications and
+      contributing to impactful products. Apart from programming, I value
+      consistency, discipline, and lifelong learning, and I enjoy sharing
+      knowledge through teaching and mentoring students.
+    </p>
+  );
+}
+export default About;
